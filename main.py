dec165e2c93a909a54ebe58b3e89d42c3b300e02
Finn Eyles <feyles@icloud.com>
2026-03-24 17:47:08 +0000

    raised password max length

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class PyObjectId(ObjectId):
 
 class RegisterRequest(BaseModel):
     username: str = Field(..., min_length=1, max_length=20)
-    password: str = Field(..., min_length=8, max_length=20)
+    password: str = Field(..., min_length=8, max_length=100)
 
 class LoginRequest(BaseModel):
     username: str
